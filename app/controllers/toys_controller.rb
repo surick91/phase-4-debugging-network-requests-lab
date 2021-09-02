df4dac1fec87a2a12391e3901281d7e2d3652d62
sam <surickj@gmail.com>
2021-09-02 14:57:01 -0700

    commit tree b7cb8800ceb5ffa3168629694b66b900dc14b530

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,7 +7,7 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
@@ -21,6 +21,12 @@ class ToysController < ApplicationController
     toy.destroy
     head :no_content
   end
+
+  def show
+    toy = Toy.find_by(id: params[:id])
+    render json: toy
+  end
+
 
   private
   
